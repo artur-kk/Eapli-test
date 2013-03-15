dac0b6db869622f77d0bd70f3a381ec93fbf3451
Artur Jorge Nunes Teixeira <artur_teixeira_5@hotmail.com>
2013-03-15 15:11:19 +0000

    Gay:João Camelo, por:

--- Eapli-test/src/Main.java
+++ Eapli-test/src/Main.java
@@ -14,5 +14,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Teste *-*");
         JOptionPane.showConfirmDialog(null, "Foi encontrado um erro na sua construção,\nvocê é gay!!!!!!!!!!", "ERRO::ÉS GAY", JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);    
+        JOptionPane.showConfirmDialog(null, "Gay és tu!", "ERRO::GAY, tipo, bué!", JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);    
     }
 }
