781a913af63c5c930b1b24adb69af61c2de1883e
Joao Camelo <Kameluh@Kameluh-PC>
2013-03-15 14:57:28 +0000

    agora é que se trabalha man

--- Eapli-test/src/Main.java
+++ Eapli-test/src/Main.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -10,5 +13,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Teste *-*");
+        JOptionPane.showMessageDialog(null, "Já foste");
     }
 }
